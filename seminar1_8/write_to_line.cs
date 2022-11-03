d24f5d780b72a0ca3489190275c6f9f09a83246b
DENIS <hindoo@mail.ru>
2022-11-04 01:55:15 +0300

    fourth exercise

---
+++ seminar1_8/write_to_line.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (-1 <= num &&  num <= 1){
+    Console.WriteLine("output nothing");
+}
+else if (num < -1){
+    for(int i = 1; i >= num; i--){
+        if (i % 2 == 0){
+            Console.WriteLine(i);
+        }
+    }   
+}
+else if (num > 1){
+    for(int i = 1; i <= num; i++){
+        if (i % 2 == 0){
+            Console.WriteLine(i);
+        }
+    }   
+}
