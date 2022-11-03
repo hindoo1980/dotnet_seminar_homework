eff18b04317c2f03e8496e2d42ee38e0be0747c5
DENIS <hindoo@mail.ru>
2022-11-04 01:23:38 +0300

    second exercise change input line

--- seminar1_4/max_of_3.cs
+++ seminar1_4/max_of_3.cs
@@ -4,7 +4,7 @@ Console.Write("input first number: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("input second number: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("input second number: ");
+Console.Write("input third number: ");
 int num3 = Convert.ToInt32(Console.ReadLine());
 
 int max = num1;
