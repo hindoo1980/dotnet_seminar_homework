9de18f89234c792d30c099947d9b1d69f0231b32
DENIS <hindoo@mail.ru>
2022-11-04 01:30:46 +0300

    third exercise

---
+++ seminar1_6/even_check.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.Write("input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0) {
+    Console.WriteLine("Да");
+}
+else {
+    Console.WriteLine("Нет");
+}
