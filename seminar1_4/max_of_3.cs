b83123394595680e7c431a00051ba2b4e4b29726
DENIS <hindoo@mail.ru>
2022-11-04 01:21:10 +0300

    second exercise

---
+++ seminar1_4/max_of_3.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("input first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input second number: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if (max < num2) {
+max = num2;
+}
+if (max < num3) {
+max = num3;
+}
+Console.WriteLine(max);
